025ae42aff77fdb6f87fb0e126a51bd59fc72eee
Uzair Khan <uzair.khan614@gmail.com>
2018-10-19 14:48:44 +0500

    Initial commit. Added sum and multiply typescript functions

---
+++ Week1Class1/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function add(num1, num2) {
+    return num1 + num2;
+}
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+let num1 = 3;
+let num2 = 5;
+console.log(`Sum of ${num1} and ${num2} is ${add(num1, num2)}`);
+console.log(`Multiplication of ${num1} and ${num2} is ${multiply(num1, num2)}`);
---
+++ Week1Class1/index.ts
@@ -0,0 +1,13 @@
+function add(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function multiply(num1: number, num2: number): number {
+  return num1 * num2;
+}
+
+let num1 = 3;
+let num2 = 5;
+
+console.log(`Sum of ${num1} and ${num2} is ${add(num1, num2)}`);
+console.log(`Multiplication of ${num1} and ${num2} is ${multiply(num1, num2)}`);
